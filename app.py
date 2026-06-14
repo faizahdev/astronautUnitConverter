0686c7a5019739a289345f56e0f034a624555cc2
Faizah Abdullah <ffaizah.ab@gmail.com>
2026-06-14 18:22:53 -0400

    added rudimentary buttons

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask 
+from flask import Flask, render_template 
 from waitress import serve 
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hi this is a test :)"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000) 
+    # serve(app, host="0.0.0.0", port=8000) 
+    app.run(debug=True)
